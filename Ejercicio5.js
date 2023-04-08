b03c039361f8276b145a1518d7eac736c572d832
Axel Rodriguez <axelrprado@gmail.com>
2023-04-08 02:31:24 -0600

    Ejercicio 5

---
+++ Ejercicio5.js
@@ -0,0 +1,8 @@
+let alturaEnCm = 168;
+let alturaaEnMts = 1.68;
+let peso = 70.68;
+let pesoRoundedUp = Math.ceil(peso);
+let pesoRoundedDown = Math.floor(peso);
+const max_valor_JS = Number.MAX_VALUE + 1;
+const isEquals =  (max_valor_JS +  1) === max_valor_JS;
+console.log(isEquals);
